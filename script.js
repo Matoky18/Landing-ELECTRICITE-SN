6a7d586fc7bc95601b2ddef8484d1c8cb395f0a5
Matoky18 <matokyranay@gmail.com>
2026-04-17 04:11:37 +0300

    test cta button sur mobile

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const ul = document.querySelector(".ul-slider-container")
 const totalSliders = document.querySelectorAll(".ul-slider-container > li")
 
-console.log(ul,totalSliders)
 
 
-ul.style
+// cta bouton 
+const ctabutton = document.querySelectorAll(".cta-button")
+    const phoneNumber = "261375078812"
+const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+console.log(ctabutton,isMobile,navigator.userAgent)
+
+ctabutton.forEach(cta => {
+
+    cta.addEventListener("click",()=>{
+    
+    if (isMobile) {
+        window.location.href = `tel:+${phoneNumber}`
+    } else {
+        window.location.href = `https://wa.me/${phoneNumber}?text=Bonjour`
+    }
+
+
+})
+});
+
+
+// cta bouton fin
