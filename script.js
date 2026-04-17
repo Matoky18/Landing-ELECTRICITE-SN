8edd40a6ba6be78eb514a5543155e0b78bf86b2d
Matoky18 <matokyranay@gmail.com>
2026-04-17 05:20:21 +0300

    numéro de téléphone changé

--- script.js
+++ script.js
@@ -5,24 +5,24 @@ const totalSliders = document.querySelectorAll(".ul-slider-container > li")
 
 // cta bouton 
 const ctabutton = document.querySelectorAll(".cta-button")
-    const phoneNumber = "261375078812"
+const phoneNumber = "33627166572"
 const isMobile = window.matchMedia("(max-width: 768px").matches;
 
-console.log(ctabutton,isMobile,navigator.userAgent,window.innerWidth)
+console.log(ctabutton, isMobile, navigator.userAgent, window.innerWidth)
 
 ctabutton.forEach(cta => {
 
-    cta.addEventListener("click",()=>{
-    
-    if (isMobile) {
-        window.location.href = `tel:+${phoneNumber}`
-    } else {
-        window.open(`https://wa.me/${phoneNumber}?text=Bonjour`);
-        console.log("desktop")
-    }
+    cta.addEventListener("click", () => {
+
+        if (isMobile) {
+            window.location.href = `tel:+${phoneNumber}`
+        } else {
+            window.open(`https://wa.me/${phoneNumber}?text=Bonjour`);
+            console.log("desktop")
+        }
 
 
-})
+    })
 });
 
 
@@ -31,8 +31,8 @@ ctabutton.forEach(cta => {
 const contactezNous = document.getElementById("contactez-nous")
 console.log(contactezNous)
 
-contactezNous.addEventListener("click",()=>{
+contactezNous.addEventListener("click", () => {
     console.log("clicked")
-    document.getElementById("footer").scrollIntoView({behavior: "smooth"})
+    document.getElementById("footer").scrollIntoView({ behavior: "smooth" })
 
 })
